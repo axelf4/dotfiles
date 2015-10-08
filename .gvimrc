6f67b0067e3ab4adf8226fba9b9c9201ceca6d19
Axel Forsman <axelsfor@gmail.com>
2015-10-08 19:31:14 +0200

    New bootstrap script. Solarized plugin.

---
+++ .gvimrc
@@ -0,0 +1,3 @@
+set guioptions-=m  " Hide menu bar
+set guioptions-=T  " Hide toolbar
+set guioptions-=r  " Hide scrollbar
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,120 @@
 #!/usr/bin/env bash
+#
+# bootstrap installs things.
 
-cd "$(dirname "${BASH_SOURCE}")";
+set -e
+DOTFILES_ROOT=$(pwd)
 
-git pull origin master;
+info () {
+	printf "  [ \033[00;34m..\033[0m ] $1"
+}
 
-rsync --exclude ".git/" --exclude "bootstrap.sh" --exclude "README.md" -avh --no-perms . ~;
+user () {
+	printf "\r  [ \033[0;33m?\033[0m ] $1 "
+}
+
+success () {
+	printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
+}
+
+fail () {
+	printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n"
+	echo ''
+	exit
+}
+
+link_file () {
+	local src=$1 dst=$2
+
+	local overwrite= backup= skip=
+	local action=
+
+	if [ -f "$dst" -o -d "$dst" -o -L "$dst" ]
+	then
+
+		if [ "$overwrite_all" == "false" ] && [ "$backup_all" == "false" ] && [ "$skip_all" == "false" ]
+		then
+
+			local currentSrc="$(readlink $dst)"
+
+			if [ "$currentSrc" == "$src" ]
+			then
+				skip=true;
+			else
+				user "File already exists: $dst ($(basename "$src")), what to do?\n[s]kip, [S]kip all, [o]verwrite, [O]verwrite all, [b]ackup, [B]ackup all?"
+				read -n 1 action
+
+				case "$action" in
+					o ) overwrite=true;;
+				O ) overwrite_all=true;;
+			b ) backup=true;;
+		B ) backup_all=true;;
+	s ) skip=true;;
+S ) skip_all=true;;
+		  * ) ;;
+	  esac
+  fi
+	fi
+
+	overwrite=${overwrite:-$overwrite_all}
+	backup=${backup:-$backup_all}
+	skip=${skip:-$skip_all}
+
+	if [ "$overwrite" == "true" ]
+	then
+		rm -rf "$dst"
+		success "Removed $dst"
+	fi
+
+	if [ "$backup" == "true" ]
+	then
+		mv "$dst" "${dst}.backup"
+		success "Moved $dst to ${dst}.backup"
+	fi
+
+	if [ "$skip" == "true" ]
+	then
+		success "Skipped $src"
+	fi
+fi
+
+if [ "$skip" != "true" ]  # "false" or empty
+then
+	mkdir -p "$(dirname "${dst}")"
+	ln -s "$1" "$2"
+	success "Linked $1 to $2"
+fi
+}
+
+install_dotfiles () {
+	info 'Installing dotfiles'
+
+	local overwrite_all=false backup_all=false skip_all=false
+
+	for src in $(find -H "$DOTFILES_ROOT" -name '.*')
+	do
+		# dst="$HOME/$(basename "${src%.*}")"
+		dst="$HOME/$(basename $src)"
+		if [ -d "$src" ]
+		then
+			mkdir -p "$dst"
+		else
+			link_file "$src" "$dst"
+		fi
+	done
+}
+
+git pull origin master
+install_dotfiles
+
+while true; do
+	read -p "Do you wish to install Vundle? " yn
+	case $yn in
+		[Yy]* ) mkdir -p "$HOME/.vim/bundle"; git clone https://github.com/VundleVim/Vundle.vim.git "$HOME/.vim/bundle/Vundle.vim"; break;;
+	[Nn]* ) break;;
+* ) echo "Please answer yes or no.";;
+	esac
+done
+
+echo ''
+echo '  All installed!'
