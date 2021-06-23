9c8212b90f47eff645854e6b4d6074f67751e32f
Axel Forsman <axelsfor@gmail.com>
2021-06-23 15:49:43 +0200

    Use motion as initial state for read-only modes
    The keys "ZZ"/"ZQ" get reserved in evil-motion-state-map for their
    normal mode commands. This means they would have to be overwritten
    individually in both the visual/operator states for actual motions to be
    bound to them. It should be noted that there is precedent for this in
    Evil; `evil-scroll-line-to-center` is bound to "zz" in motion state.
    
    evil-collection instead uses normal state for this purpose and remaps
    insertion commands to be ignored. While less obtrusive, this requires
    rebinding all the default keys for each read-only mode, which is too
    tedious.

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -2,30 +2,32 @@
 
 ;; Bootstrap the straight.el package manager
 (load (expand-file-name "straight/repos/straight.el/bootstrap.el"
-						user-emacs-directory) nil 'nomessage)
+                        user-emacs-directory) nil 'nomessage)
 
 (setq gc-cons-threshold 16777216
-	  enable-recursive-minibuffers t
-	  scroll-conservatively most-positive-fixnum ; Do not center cursor after scrolling
-	  truncate-partial-width-windows nil ; Always soft-wrap
-	  select-enable-clipboard nil ; Do not tie unnamed register "" to system clipboard
-	  sentence-end-double-space nil ; Single space between sentences
-	  show-paren-delay 0
-	  make-backup-files nil
-	  auto-save-no-message t
-	  vc-handled-backends nil) ; Disable VC
+      enable-recursive-minibuffers t
+      scroll-conservatively most-positive-fixnum ; Do not center cursor after scrolling
+      truncate-partial-width-windows nil ; Always soft-wrap
+      select-enable-clipboard nil ; Do not tie unnamed register "" to system clipboard
+      sentence-end-double-space nil ; Single space between sentences
+      show-paren-delay 0
+      make-backup-files nil
+      auto-save-no-message t
+      tags-revert-without-query t
+      vc-handled-backends nil) ; Disable VC
 (setq-default tab-width 4)
-(menu-bar-mode -1) ; Disable the menu bar
-(show-paren-mode 1) ; Highlight matching parentheses
+(menu-bar-mode 0) ; Disable the menu bar
+(show-paren-mode) ; Highlight matching parentheses
+(global-auto-revert-mode)
 (global-set-key [escape] 'keyboard-escape-quit)
 (fset 'yes-or-no-p 'y-or-n-p)
 
 (electric-pair-mode) ; Autopairing
 ;; Inhibit autopairing in minibuffers
 (add-hook 'minibuffer-setup-hook
-          (lambda () (setq-local electric-pair-inhibit-predicate 'identity)))
+          (lambda () (setq-local electric-pair-inhibit-predicate #'identity)))
 
-;; vi emulation
+;;; vi emulation
 (straight-use-package 'evil)
 (straight-use-package 'undo-tree)
 (straight-use-package 'goto-chg)
@@ -42,20 +44,23 @@
 
  evil-undo-system 'undo-tree
  evil-want-Y-yank-to-eol t ; Make Y consistent with other capitals
+ evil-symbol-word-search t
  evil-split-window-below t evil-vsplit-window-right t)
-
-(evil-mode 1)
-(evil-set-leader 'normal (kbd "SPC"))
+(evil-mode)
+(evil-set-leader 'motion (kbd "SPC"))
 (add-hook 'evil-local-mode-hook 'undo-tree-mode)
 (evil-define-key 'normal 'global
-  (kbd "C-a") 'evil-numbers/inc-at-pt
-  (kbd "C-x") 'evil-numbers/dec-at-pt)
+  "\C-a" 'evil-numbers/inc-at-pt "\C-x" 'evil-numbers/dec-at-pt
+  (kbd "g C-a") 'evil-numbers/inc-at-pt-incremental
+  (kbd "g C-x") 'evil-numbers/dec-at-pt-incremental)
+;; Bindings that make motion state usable as initial state for read-only modes
+(evil-define-key 'motion 'global "ZZ" 'evil-save-modified-and-close "ZQ" 'evil-quit)
 
 ;; Inherit command-line mappings in minibuffers
 (set-keymap-parent minibuffer-local-map evil-ex-completion-map)
 (define-key minibuffer-local-map [remap completion-at-point] nil) ; but undo remapping...
 ;; Make Evil motions/text objects always use the default paragraph definition
-(advice-add 'forward-evil-paragraph :around
+(advice-add #'forward-evil-paragraph :around
             (lambda (orig-fun &rest args)
               (let ((paragraph-start (default-value 'paragraph-start))
                     (paragraph-separate (default-value 'paragraph-separate)))
@@ -71,9 +76,12 @@
             (setq last-command this-command)
             (goto-last-change arg)))))
 
-(evil-set-initial-state 'help-mode 'normal)
-(evil-define-key 'normal help-mode-map (kbd "C-t") 'help-go-back)
-(define-key key-translation-map (kbd "<leader>h") "\C-h")
+(evil-set-initial-state 'messages-buffer-mode 'motion)
+;; ...and the preexistent "*Messages*" buffer
+(with-current-buffer (messages-buffer) (evil-motion-state))
+
+(global-set-key (kbd "<leader>h") 'help-command)
+(evil-define-key 'motion help-mode-map (kbd "C-t") 'help-go-back)
 
 ;; System clipboard support while running in terminal
 (straight-use-package 'xclip)
@@ -83,16 +91,16 @@
   "Call FUN with ARGS and restore the kill ring afterward."
   (let ((kill-ring kill-ring)) (apply fun args)))
 ;; Pasting from system clipboard should not also copy the pasted text
-(advice-add 'current-kill :around #'save-kill-ring)
+(advice-add #'current-kill :around #'save-kill-ring)
 
-;; Fuzzy finding
+;;; Minibuffer completion
 (straight-use-package 'selectrum)
 (straight-use-package 'selectrum-prescient)
-(selectrum-mode 1)
-(selectrum-prescient-mode 1)
+(selectrum-mode)
+(selectrum-prescient-mode)
 (setq selectrum-extend-current-candidate-highlight t)
 
-;; Project management
+;;; Project management
 (set-frame-parameter nil 'cwd default-directory) ; For the initial frame
 (push 'cwd frame-inherited-parameters)
 (defun project-root ()
@@ -101,7 +109,7 @@
 (add-hook
  'server-after-make-frame-hook
  (lambda ()
-   (let ((client (frame-parameter nil 'client)))
+   (when-let ((client (frame-parameter nil 'client)))
      (set-frame-parameter nil 'cwd (process-get client 'server-client-directory))
      ;; find-file-hook for files visited by client runs before frame creation
      (dolist (buf (process-get client 'buffers))
@@ -117,16 +125,17 @@
     (let ((default-directory (project-root)))
       (find-file
        (completing-read
-        "Find file: " (apply 'process-lines find-files-program))))))
+        "Find file: " (apply #'process-lines find-files-program))))))
 
-(evil-define-key 'normal 'global
-  (kbd "<leader>f") 'find-file-rec)
+(evil-define-key 'motion 'global
+  (kbd "<leader>f") 'find-file-rec
+  (kbd "<leader>b") 'switch-to-buffer)
 
-;; Customize mode line
+;;; Customize mode line
 (setq-default
  mode-line-buffer-identification
  '(:eval
-   (if (buffer-file-name)
+   (if buffer-file-name
        (let* ((dir (abbreviate-file-name
                     (string-remove-prefix (project-root)
                                           (file-name-directory buffer-file-name))))
@@ -134,24 +143,26 @@
          (list
           (if (< (length parts) 5) dir
             (string-join `(,(car parts) "..." . ,(last parts 2)) "/"))
-          (propertize (file-name-nondirectory (buffer-file-name))
+          (propertize (file-name-nondirectory buffer-file-name)
                       'face 'mode-line-buffer-id)))
      (propertize (buffer-name) 'face 'mode-line-buffer-id)))
  mode-line-modified '(:eval (when (buffer-modified-p) " [+]"))
+ mode-line-position "%l,%C"
  mode-line-format
  '("%e " ; Out-of-memory indication
    mode-line-buffer-identification mode-line-modified
    ;; Right-justified line and column number
-   (:eval (let* ((rhs (format-mode-line "%l,%C"))
+   (:eval (let* ((rhs (format-mode-line mode-line-position))
                  (hpos (- (window-width) (string-width rhs) 1)))
             (list (propertize " " 'display `(space :align-to ,hpos)) rhs)))))
 
-;; Colorscheme
+;;; Colorscheme
 (straight-use-package 'gruvbox-theme)
 (load-theme 'gruvbox t)
 
-;; Language support
-(add-hook 'emacs-lisp-mode-hook (lambda () (setq indent-tabs-mode nil)))
+;;; Language support
+(add-hook 'emacs-lisp-mode-hook (lambda () (setq tab-width 8
+                                                 indent-tabs-mode nil)))
 
 (straight-use-package 'rust-mode)
 (setq rust-format-on-save t)
@@ -159,5 +170,5 @@
 		  (lambda () (setq indent-tabs-mode nil)))
 
 (straight-use-package 'haskell-mode)
-
+(straight-use-package 'yaml-mode)
 (straight-use-package 'nix-mode)
